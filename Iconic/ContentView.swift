0ed785ee9d8a257c04eac0fabfa7d711438b4c15
Himank Mahani <HimankMahani@users.noreply.github.com>
2026-05-30 08:11:45 +0530

    Initial Commit

---
+++ Iconic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Iconic
+//
+//  Created by Himank Mahani on 30/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Iconic/IconicApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconicApp.swift
+//  Iconic
+//
+//  Created by Himank Mahani on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct IconicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
